995348872014bd01449e25026e18bb12e3ea41c7
Wendrew <wendrew.douglas@gmail.com>
2021-12-29 18:34:57 -0300

    atualizando apenas arquivo

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-echo "Olá Mundo! 456";
+echo "Olá Mundo! 123456";
 
-echo "Ulá Udemy"
-=======
-echo "Olá Mundo! 123";
+echo "Ulá Udemy e Hcode"
 
-echo "Olá HCode...";
->>>>>>> de6810388768b565a28c771d98a786c547ed963d
-
->
+?>
